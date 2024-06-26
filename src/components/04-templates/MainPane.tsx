c1225120b9ddb2a1b52763f1be7cbe4ac74e202d
heronlancellot.eth <heronlancellot@id.uff.br>
2024-06-26 17:11:06 -0300

    update: main-pane component

--- src/components/04-templates/MainPane.tsx
+++ src/components/04-templates/MainPane.tsx
@@ -1,60 +1,19 @@
-// components/MainPane.tsx
 import { type FC } from "react";
 
-import { Box, Divider, Flex, Heading, useColorMode } from "@chakra-ui/react";
+import { ConnectButton } from "@rainbow-me/rainbowkit";
 import { useAccount } from "wagmi";
-
-import { SignMessage } from "@/components/01-atoms";
-import {
-  Status,
-  Address,
-  Chain,
-  Balance,
-  BlockNumber,
-  TransferNative,
-} from "@/components/02-molecules";
-import styles from "@/styles/mainPane.module.css";
 
 export const MainPane: FC = () => {
   const { isConnected } = useAccount();
-  const { colorMode } = useColorMode();
 
   return (
-    <Box
-      className={styles.container}
-      border={
-        colorMode === "light" ? "none" : "1px solid rgba(152, 161, 192, 0.24)"
-      }
-    >
-      <Heading as="h2" fontSize={"2rem"} mb={10} className="text-shadow">
-        Display Info
-      </Heading>
-
-      <Flex className={styles.content}>
-        <Status />
-
-        {isConnected && (
-          <>
-            <Address />
-            <Chain />
-            <Balance />
-            <BlockNumber />
-
-            <Divider mb={5} />
-
-            <Flex
-              w={"100%"}
-              display={"flex"}
-              justifyContent={"space-around"}
-              flexWrap={"wrap"}
-              gap={5}
-            >
-              <SignMessage />
-              <TransferNative />
-            </Flex>
-          </>
-        )}
-      </Flex>
-    </Box>
+    <div className="w-full h-full flex flex-col justify-center items-center gap-10">
+      <div className="flex">
+        <h1>Online reputation made easy</h1>
+      </div>
+      <div className="flex">
+        {!isConnected && <ConnectButton label="Connect" />}
+      </div>
+    </div>
   );
 };
