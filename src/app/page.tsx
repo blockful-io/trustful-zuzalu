25c990f083a989ca85a3d1898c6f0180c52c26ab
heronlancellot <heronlancellot@id.uff.br>
2024-06-28 19:04:35 -0300

    feat: add home-section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,19 +1,11 @@
 "use client";
-import { Box, Flex } from "@chakra-ui/react";
 
-import { Footer, Header } from "@/components/01-atoms";
-import { MainPane } from "@/components/04-templates";
+import { HomeSection } from "@/components/04-templates/";
 
 export default function Home() {
   return (
-    <Flex flexDirection="column" minHeight="100vh">
-      <Header />
-
-      <Box as="main" flex={1} p={4}>
-        <MainPane />
-      </Box>
-
-      <Footer />
-    </Flex>
+    <div className="flex flex-col min-h-screen">
+      <HomeSection />
+    </div>
   );
 }
