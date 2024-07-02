3ea47c14e0989430d8e19011e33eda3e331c4609
heronlancellot.eth <heronlancellot@id.uff.br>
2024-07-02 12:32:40 -0300

    update: The Footer mobile

--- src/components/01-atoms/TheFooter.tsx
+++ src/components/01-atoms/TheFooter.tsx
@@ -1,26 +1,33 @@
 "use client";
 import { type FC } from "react";
 
-import { Box } from "@chakra-ui/react";
-import Link from "next/link";
+import { Box, Flex } from "@chakra-ui/react";
+import { useAccount } from "wagmi";
+
+import { useWindowSize } from "@/hooks";
+
+import { BlockfulLogo } from "./icons/BlockfulLogo";
 
 export const TheFooter: FC = () => {
+  const { isMobile } = useWindowSize();
+  const { isConnected } = useAccount();
   return (
     <Box
       as="footer"
-      p={"1rem"}
       position="sticky"
       bottom={0}
       zIndex={10}
       textAlign={"center"}
+      className="px-6 py-7 sm:p-0"
     >
-      <Link
-        href="https://github.com/Pedrojok01/Next-Web3-Boilerplate"
-        target="_blank"
-        rel="noopener noreferrer"
-      >
-        Don&apos;t forget to leave a ⭐️ on this boilerplate if you like it!
-      </Link>
+      {isMobile && !isConnected && (
+        <Flex direction={"column"} gap={2}>
+          <p className="text-xs font-medium leading-3 uppercase tracking-wider text-gray-200 opacity-50 text-left">
+            Created by
+          </p>
+          <BlockfulLogo />
+        </Flex>
+      )}
     </Box>
   );
 };
