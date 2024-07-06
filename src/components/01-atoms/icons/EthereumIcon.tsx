27a15d5047d72797e3bc894598d50d723c63819a
heronlancellot.eth <heronlancellot@id.uff.br>
2024-07-05 21:31:34 -0300

    update: icon svg variable name

--- src/components/01-atoms/icons/EthereumIcon.tsx
+++ src/components/01-atoms/icons/EthereumIcon.tsx
@@ -15,7 +15,7 @@ export const EthereumIcon = (props: SVGProps<SVGSVGElement>) => {
           id="Vector"
           d="M15.8711 10.1875L9.9375 13.8125L4 10.1875L9.9375 0L15.8711 10.1875ZM9.9375 14.9766L4 11.3516L9.9375 20L15.875 11.3516L9.9375 14.9766Z"
           fill="#F5FFFF"
-          fill-opacity="0.35"
+          fillOpacity="0.35"
         />
       </g>
     </svg>
