9e7d2d986c8b4930e19c65fc3a675195dcce11f1
RafaDSan <rafaeldss12@gmail.com>
2024-07-10 15:36:49 -0300

    feat: Fetching attestations data but need some refinement

--- src/components/04-templates/PreCheckinSection.tsx
+++ src/components/04-templates/PreCheckinSection.tsx
@@ -1,33 +1,67 @@
 import { Box, Button, Flex } from "@chakra-ui/react";
 import { useRouter } from "next/navigation";
-import { useAccount } from "wagmi";
+// import { useAccount } from "wagmi";
 
 import { TheHeader } from "@/components/01-atoms";
 
 import useFetchEASData from "../../hooks/useFetchEASData";
 import { VILLAGER_QUERY } from "../../lib/client/schema_queries";
+import { ethers } from "ethers";
 
-//TODO - In progress. Hide check-in button if `checked-in`status is present in the attestation
+//OBS - Code working but could use some refinement
+//TODO - Adjust typing
 export const PreCheckinSection = () => {
   const router = useRouter();
-
+  const abiCoder = new ethers.AbiCoder();
   // const { address /*isConnected*/ } = useAccount();
 
+  //variable to be passed to the fetchEASdata hook
   const VARIABLES = {
     where: {
-      id: "0xd130b9591f22bb9653f125ed00ff2d7d88b41d64acfd962365b42fe720c295aa", //Manager - Trocar pro villager
+      id: "0xcb74f95269512c5cb95c9f854a8ec8fe015d453f1bbff52d83ffd894dfec1883",
     },
     attestationsWhere2: {
       recipient: {
-        equals: "0x07231e0fd9F668d4aaFaE7A5D5f432B8E6e4Fe51",
+        equals: "0x9bc0fD0Dd492437D6217A12807E81b4A72399379", //Use `address` to get the signed in user's address
       },
     },
   };
+  //Decode function to decode an element from the `attestationsArray`
+  const decodeAttestationData = (encodedData) => {
+    try {
+      const decoded = abiCoder.decode(["string"], encodedData);
+      return decoded[0];
+    } catch (error) {
+      console.error("Error decoding attestation data:", error);
+      return null;
+    }
+  };
 
+  //Destructuring the result of useFetchEASData to `data`
   const { data } = useFetchEASData(VILLAGER_QUERY, VARIABLES);
-  const attestationsArray = data?.data?.schema?.attestations ?? [];
-  console.log("data", data);
-  console.log("attestationsArray", attestationsArray);
+
+  //"Cleaning" the nested object into a single array
+  const attestationsArray = data?.data.schema?.attestations || [];
+
+  //Creating a decoded array
+  const decodedAttestationsArray = attestationsArray.map((attestation) =>
+    decodeAttestationData(attestation.data),
+  );
+
+  const hasCheckinAttestation = decodedAttestationsArray.some(
+    (decodedData) => decodedData && decodedData.includes("Check-in"),
+  );
+
+  const hasCheckoutAttestation = decodedAttestationsArray.some(
+    (decodedData) => decodedData && decodedData.includes("Check-out"),
+  );
+  //Passing each element of the decoded array to be validaded regarding its data value
+  const isCheckedIn = decodedAttestationsArray.some(
+    (decodedData) =>
+      decodedData && hasCheckinAttestation && !hasCheckoutAttestation,
+  );
+
+  const hasBothAttestations = hasCheckinAttestation && hasCheckoutAttestation;
 
   return (
     <Flex flexDirection="column" minHeight="100vh">
@@ -42,11 +76,16 @@ export const PreCheckinSection = () => {
         <Flex flexDirection={"column"} gap={2}>
           <Button onClick={() => router.push("my-badge")}>My Badges</Button>
           <Button onClick={() => router.push("give-badge")}>Give Badges</Button>
-          <Button onClick={() => router.push("check-in")}>
-            {" "}
-            Check In
-          </Button>{" "}
-          {/*Change to Check out if user has Check in badge */}
+
+          {!hasBothAttestations && (
+            <Button
+              onClick={() =>
+                router.push(isCheckedIn ? "check-out" : "check-in")
+              }
+            >
+              {isCheckedIn ? "Check Out" : "Check In"}
+            </Button>
+          )}
         </Flex>
       </Box>
     </Flex>
