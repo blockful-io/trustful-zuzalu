30c94be4674c4558f8217613c028cc61d509f699
0xneves <asguilift@hotmail.com>
2024-07-10 19:10:40 +0700

    fix: case where address is undefined

--- src/utils/formatters.ts
+++ src/utils/formatters.ts
@@ -1,9 +1,9 @@
 export const getEllipsedAddress = (
-  str: `0x${string}`,
+  str: `0x${string}` | undefined,
   n: number = 6,
 ): string => {
   if (str) {
     return `${str.slice(0, n)}...${str.slice(str.length - (n - 2))}`;
   }
-  return "";
+  return "Undefined Address";
 };
