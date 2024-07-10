5dd91c259c752e4acc2de8cd14dd0e4e23bcc95a
RafaDSan <rafaeldss12@gmail.com>
2024-07-10 03:28:05 -0300

    feat: (inProgress) hide checkIn button and fetch data

--- src/components/04-templates/PreCheckinSection.tsx
+++ src/components/04-templates/PreCheckinSection.tsx
@@ -1,10 +1,25 @@
 import { Box, Button, Flex } from "@chakra-ui/react";
 import { useRouter } from "next/navigation";
+import { useAccount } from "wagmi";
 
 import { TheHeader } from "@/components/01-atoms";
 
+import useFetchEASData from "../../hooks/useFetchEASData";
+import { VILLAGER_QUERY } from "../../lib/client/schema_queries";
+
+//TODO - In progress. Hide check-in button if `checked-in`status is present in the attestation
 export const PreCheckinSection = () => {
   const router = useRouter();
+
+  const { address /*isConnected*/ } = useAccount();
+
+  const VARIABLES = {
+    where: "0xd130b9591f22bb9653f125ed00ff2d7d88b41d64acfd962365b42fe720c295aa", //Temporarly harcoded
+    attestationsWhere2: address,
+  };
+
+  const { data } = useFetchEASData(VILLAGER_QUERY, VARIABLES);
+  console.log("data", data);
 
   return (
     <Flex flexDirection="column" minHeight="100vh">
