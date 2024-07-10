d1bc20183749efdb51a426483b3c9e5a7e88fdd0
RafaDSan <rafaeldss12@gmail.com>
2024-07-10 15:39:57 -0300

    feat: Mocked queries created (still in progress)

--- src/lib/client/schema_queries.ts
+++ src/lib/client/schema_queries.ts
@@ -6,3 +6,33 @@ $attestationsWhere2: AttestationWhereInput) {
     }
   }
 }`;
+
+//MODIFY
+export const MANAGER_QUERY = `query Query($where: SchemaWhereUniqueInput!, 
+$attestationsWhere2: AttestationWhereInput) {
+  schema(where: $where) {
+    attestations(where: $attestationsWhere2) {
+      data
+    }
+  }
+}`;
+
+//MODIFY
+export const ATTEST_QUERY = `query Query($where: SchemaWhereUniqueInput!, 
+$attestationsWhere2: AttestationWhereInput) {
+  schema(where: $where) {
+    attestations(where: $attestationsWhere2) {
+      data
+    }
+  }
+}`;
+
+//MODIFY
+export const ATTEST_RESPONSE_QUERY = `query Query($where: SchemaWhereUniqueInput!, 
+$attestationsWhere2: AttestationWhereInput) {
+  schema(where: $where) {
+    attestations(where: $attestationsWhere2) {
+      data
+    }
+  }
+}`;
