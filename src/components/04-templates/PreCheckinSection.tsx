656960b504b7d173ceda8f364bd4b6c9e96881fa
0xneves <asguilift@hotmail.com>
2024-07-12 08:54:42 +0700

    feat: blockful propaganda and optm

--- src/components/04-templates/PreCheckinSection.tsx
+++ src/components/04-templates/PreCheckinSection.tsx
@@ -4,61 +4,53 @@ import { useRouter } from "next/navigation";
 
 import { TheFooterNavbar, TheHeader } from "@/components/01-atoms";
 import { useWindowSize } from "@/hooks";
+import { useContext } from "react";
+import { WalletContext } from "@/lib/context/WalletContext";
 
 export const PreCheckInSection = () => {
   const { push } = useRouter();
   const { isMobile } = useWindowSize();
 
+  const { villagerAttestationCount } = useContext(WalletContext);
+
   return (
     <Flex flexDirection="column" minHeight="100vh">
       <TheHeader />
-
-      {isMobile && (
-        <Button
-          flex="1"
-          padding="0"
-          onClick={() => push("/checkin")}
-          display="flex"
-          justifyContent="center"
-          alignItems="center"
-          color={"transparent"}
-          backgroundColor={"transparent"}
-          _hover={{ bg: "transparent" }}
-          _active={{ bg: "transparent" }}
-        >
-          <Image
-            src="/img/zuvillage.png"
-            alt="ZuVillage Georgia Have Fun"
-            objectFit="cover"
-            width={300}
-            height={300}
-          />
-        </Button>
-      )}
-
-      {!isMobile && (
-        <Button
-          flex="1"
-          padding="0"
-          onClick={() => push("/checkin")}
-          display="flex"
-          justifyContent="center"
-          alignItems="center"
-          color={"transparent"}
-          backgroundColor={"transparent"}
-          _hover={{ bg: "transparent" }}
-          _active={{ bg: "transparent" }}
-        >
-          <Image
-            src="/img/zuvillage.png"
-            alt="ZuVillage Georgia Have Fun"
-            objectFit="cover"
-            width={300}
-            height={300}
-          />
-        </Button>
-      )}
-
+      <Button
+        flex="1"
+        padding="0"
+        onClick={() => push("/checkin")}
+        display="flex"
+        justifyContent="center"
+        alignItems="center"
+        color={"transparent"}
+        backgroundColor={"transparent"}
+        _hover={{ bg: "transparent" }}
+        _active={{ bg: "transparent" }}
+      >
+        {isMobile &&
+          villagerAttestationCount &&
+          villagerAttestationCount == 0 && (
+            <Image
+              src="/img/zuvillage.png"
+              alt="ZuVillage Georgia Have Fun"
+              objectFit="cover"
+              width={300}
+              height={300}
+            />
+          )}
+        {!isMobile &&
+          villagerAttestationCount &&
+          villagerAttestationCount === 0 && (
+            <Image
+              src="/img/zuvillage.png"
+              alt="ZuVillage Georgia Have Fun"
+              objectFit="cover"
+              width={300}
+              height={300}
+            />
+          )}
+      </Button>
       <TheFooterNavbar />
     </Flex>
   );
