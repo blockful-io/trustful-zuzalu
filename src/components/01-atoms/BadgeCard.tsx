73ab22cc5f2ea7e05fb31c88204c343b6669beed
beg <andreza@noxfi.com>
2024-07-12 18:45:53 -0300

    feat: badge query list

--- src/components/01-atoms/BadgeCard.tsx
+++ src/components/01-atoms/BadgeCard.tsx
@@ -3,7 +3,6 @@ import {
   SimpleGrid,
   Card,
   CardHeader,
-  Heading,
   CardBody,
   Text,
   Divider,
@@ -20,106 +19,85 @@ import {
   CalendarIcon,
   HeartIcon,
 } from "@/components/01-atoms";
+import { getEllipsedAddress } from "@/utils/formatters";
 
-export const BadgeCard = () => {
+interface BadgeData {
+  id: string;
+  title: string;
+  status: string;
+  comment: string;
+  timeCreated: number;
+  attester: string;
+}
+
+interface BadgeCardProps {
+  badgeData: BadgeData[];
+}
+
+export const BadgeCard: React.FC<BadgeCardProps> = ({ badgeData }) => {
   const router = useRouter();
   return (
     <SimpleGrid
       spacing={4}
       templateColumns="repeat(auto-fill, minmax(255px, 1fr))"
     >
-      <Card
-        className="cursor-pointer"
-        background={"#F5FFFF0D"}
-        border={2}
-        onClick={() => {
-          router.push(`my-badge-details`); //TODO: Replace with dynamic route ID
-          console.log("Card Clicked go to Details of this Card");
-        }}
-      >
-        <CardHeader
-          gap={4}
-          display={"flex"}
-          alignItems={"center"}
-          justifyContent={"space-between"}
+      {badgeData.map((badge) => (
+        <Card
+          key={badge.id}
+          className="cursor-pointer"
+          background={"#F5FFFF0D"}
+          border={2}
+          onClick={() => {
+            router.push(`my-badge-details`); //TODO: Replace with dynamic route ID
+            console.log("Card Clicked go to Details of this Card");
+          }}
         >
-          <Flex gap={4} className={"items-center"}>
-            <HeartIcon className="w-4 h-4 opacity-50" />
-            <Heading size="md">Change My Mind</Heading>
-          </Flex>
-          <Flex className={"items-center"} gap={2}>
-            <BadgeTagIcon status={BadgeStatus.PENDING} />
-            <ArrowIcon variant={ArrowIconVariant.RIGHT} />
-          </Flex>
-        </CardHeader>
-        <Divider color={"#F5FFFF14"} />
-        <CardBody gap={4} display={"flex"} flexDirection={"column"}>
-          <Flex gap={4} alignItems={"center"} flexDirection={"row"}>
-            <CalendarIcon className="w-6 h-6" />
-            <Flex gap={2} alignItems={"center"}>
-              <Text className="text-slate-50 opacity-70 text-sm font-normal  leading-tight">
-                Date
+          <CardHeader
+            gap={4}
+            display={"flex"}
+            alignItems={"center"}
+            justifyContent={"space-between"}
+            pt="0.75rem"
+            pb="0.75rem"
+          >
+            <Flex gap={4} className={"items-center"}>
+              <HeartIcon className="w-4 h-4 opacity-50 text-slate-50" />
+              <Text fontSize="md" className="text-slate-50">
+                {badge.title}
               </Text>
-              <Text>Jun 19th - 3:32pm</Text>
             </Flex>
-          </Flex>
-          <Flex gap={4} alignItems={"center"} flexDirection={"row"}>
-            <Avatar className="w-6 h-6" />
-            <Flex gap={2} alignItems={"center"}>
-              <Text className="text-slate-50 opacity-70 text-sm font-normal  leading-tight">
-                Issued by
-              </Text>
-              <Text>lil_penguin.eth</Text>
+            <Flex className={"items-center"} gap={2}>
+              <BadgeTagIcon status={BadgeStatus.PENDING} />
+              <ArrowIcon variant={ArrowIconVariant.RIGHT} />
             </Flex>
-          </Flex>
-        </CardBody>
-      </Card>
-      <Card
-        className="cursor-pointer"
-        background={"#F5FFFF0D"}
-        border={2}
-        onClick={() => {
-          router.push(`my-badge-details`); //TODO: Replace with dynamic route ID
-          console.log("Card Clicked go to Details of this Card");
-        }}
-      >
-        <CardHeader
-          gap={4}
-          display={"flex"}
-          alignItems={"center"}
-          justifyContent={"space-between"}
-        >
-          <Flex gap={4} className={"items-center"}>
-            <HeartIcon className="w-4 h-4 opacity-5" />
-            <Heading size="md">CheckIn</Heading>
-          </Flex>
-          <Flex className={"items-center"} gap={2}>
-            <BadgeTagIcon status={BadgeStatus.CONFIRMED} />
-            <ArrowIcon variant={ArrowIconVariant.RIGHT} />
-          </Flex>
-        </CardHeader>
-        <Divider color={"#F5FFFF14"} />
-        <CardBody gap={4} display={"flex"} flexDirection={"column"}>
-          <Flex gap={4} alignItems={"center"} flexDirection={"row"}>
-            <CalendarIcon className="w-6 h-6" />
-            <Flex gap={2} alignItems={"center"}>
-              <Text className="text-slate-50 opacity-70 text-sm font-normal  leading-tight">
-                Date
-              </Text>
-              <Text>Jun 19th - 3:32pm</Text>
+          </CardHeader>
+          <Divider color={"#F5FFFF14"} />
+          <CardBody gap={4} display={"flex"} flexDirection={"column"}>
+            <Flex gap={4} alignItems={"center"} flexDirection={"row"}>
+              <CalendarIcon className="w-5 h-5 " />
+              <Flex gap={2} alignItems={"center"}>
+                <Text className="text-slate-50 opacity-70 text-sm font-normal leading-tight">
+                  Date
+                </Text>
+                <Text className="text-slate-50 text-sm font-normal ">
+                  {new Date(badge.timeCreated * 1000).toLocaleString()}
+                </Text>
+              </Flex>
             </Flex>
-          </Flex>
-          <Flex gap={4} alignItems={"center"} flexDirection={"row"}>
-            <Avatar className="w-6 h-6" />
-            <Flex gap={2} alignItems={"center"}>
-              <Text className="text-slate-50 opacity-70 text-sm font-normal  leading-tight">
-                Issued by
-              </Text>
-              <Text>crazy_monkey.eth</Text>
+            <Flex gap={4} alignItems={"center"} flexDirection={"row"}>
+              <Avatar className="w-5 h-5" />
+              <Flex gap={2} alignItems={"center"}>
+                <Text className="text-slate-50 opacity-70 text-sm font-normal leading-tight">
+                  Issued by
+                </Text>
+                <Text className="text-slate-50 text-sm font-normal ">
+                  {getEllipsedAddress(badge.attester as `0x${string}`)}
+                </Text>
+              </Flex>
             </Flex>
-          </Flex>
-        </CardBody>
-      </Card>
+          </CardBody>
+        </Card>
+      ))}
     </SimpleGrid>
   );
 };
--- src/components/04-templates/MyBadgeSection.tsx
+++ src/components/04-templates/MyBadgeSection.tsx
@@ -1,12 +1,140 @@
-import { Box, Flex } from "@chakra-ui/react";
+import React, { useEffect, useState } from "react";
+
+import { Box, Flex, Image } from "@chakra-ui/react";
+import { useAccount } from "wagmi";
 
 import { BadgeCard, TheHeader, TheFooterNavbar } from "@/components/01-atoms";
+import { useNotify } from "@/hooks/useNotify";
+import { ZUVILLAGE_SCHEMAS } from "@/lib/client/constants";
+import { BADGE_QUERY } from "@/lib/client/schemaQueries";
+import { fetchEASData } from "@/lib/service/fetchEASData";
 
-export const MyBadgeSection = () => {
+interface Attestation {
+  decodedDataJson: string;
+  timeCreated: number;
+  attester: string;
+  id: string;
+}
+
+interface BadgeData {
+  id: string;
+  title: string;
+  status: string;
+  comment: string;
+  timeCreated: number;
+  attester: string;
+}
+
+export const MyBadgeSection: React.FC = () => {
+  const { address } = useAccount();
+  const { notifyError } = useNotify();
+  const [badgeData, setBadgeData] = useState<BadgeData[]>([]);
+  const [loading, setLoading] = useState<boolean>(false);
+
+  useEffect(() => {
+    if (address) {
+      fetchData();
+    }
+  }, [address]);
+
+  const fetchData = async () => {
+    setLoading(true);
+    const response = await handleQuery();
+    if (response) {
+      const decodedData = response
+        .filter((attestation: Attestation) => attestation.decodedDataJson)
+        .map((attestation: Attestation) => {
+          const parsedJson = JSON.parse(attestation.decodedDataJson);
+
+          let title = null;
+          title = parsedJson.find((item: any) => item.name === "title")?.value
+            .value;
+          if (!title) {
+            title = parsedJson.find((item: any) => item.name === "status")
+              ?.value.value;
+            if (!title) {
+              title = parsedJson.find((item: any) => item.name === "role")
+                ?.value.value;
+            }
+          }
+          const comment = parsedJson.find(
+            (item: any) => item.name === "comment",
+          )?.value.value;
+          const id = attestation.id;
+          return {
+            id,
+            title,
+            comment,
+            timeCreated: attestation.timeCreated,
+            attester: attestation.attester,
+          };
+        });
+
+      setBadgeData(decodedData);
+    }
+    setLoading(false);
+  };
+
+  const handleQuery = async () => {
+    const queryVariables = {
+      where: {
+        OR: [
+          {
+            schemaId: {
+              equals: ZUVILLAGE_SCHEMAS[2].uid,
+            },
+            recipient: {
+              equals: address,
+            },
+          },
+          {
+            schemaId: {
+              equals: ZUVILLAGE_SCHEMAS[1].uid,
+            },
+            recipient: {
+              equals: address,
+            },
+          },
+          {
+            schemaId: {
+              equals: ZUVILLAGE_SCHEMAS[0].uid,
+            },
+            recipient: {
+              equals: address,
+            },
+          },
+        ],
+      },
+      orderBy: {
+        timeCreated: "desc",
+      },
+    };
+
+    try {
+      const { response } = await fetchEASData(BADGE_QUERY, queryVariables);
+      const attestations = response?.data?.data.attestations;
+      if (!attestations) {
+        notifyError({
+          title: "Cannot fetch EAS",
+          message: "Subgraph returned error with current query",
+        });
+        return null;
+      }
+
+      return attestations;
+    } catch (error) {
+      notifyError({
+        title: "Cannot fetch EAS",
+        message: "Error while fetching Attestation data from Subgraphs",
+      });
+      console.error("Error in handleQuery:", error);
+      return null;
+    }
+  };
+
   return (
     <Flex flexDirection="column" minHeight="100vh">
       <TheHeader />
-
       <Box
         flex={1}
         as="main"
@@ -14,10 +142,18 @@ export const MyBadgeSection = () => {
         marginBottom="60px"
       >
         <Flex flexDirection={"column"} gap={2} className="w-full">
-          <BadgeCard />
+          {loading ? (
+            <Image
+              src="/img/loading.gif"
+              width="30px"
+              margin="auto"
+              alt="Loading..."
+            />
+          ) : (
+            <BadgeCard badgeData={badgeData} />
+          )}
         </Flex>
       </Box>
-
       <TheFooterNavbar />
     </Flex>
   );
