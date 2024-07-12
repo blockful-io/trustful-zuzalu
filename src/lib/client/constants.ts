f6b8411ba14b2984a8c4d49594da392cef38d896
RafaDSan <rafaeldss12@gmail.com>
2024-07-11 21:28:42 -0300

    refactor: removed unsued lines

--- src/lib/client/constants.ts
+++ src/lib/client/constants.ts
@@ -1,60 +1,7 @@
-export enum DeployedAddresses {
-  OPSEPOLIA_TRUSTFUL_RESOLVER = "OPSEPOLIA_TRUSTFUL_RESOLVER",
-  OPTIMISM_TRUSTFUL_RESOLVER = "OPTIMISM_TRUSTFUL_RESOLVER",
-  OPTIMISM_EAS = "OPTIMISM_EAS",
-  OPTIMISM_REGISTRY = "OPTIMISM_REGISTRY",
-  OPTIMISM_SEPOLIA_EAS = "OPTIMISM_SEPOLIA_EAS",
-  OPTIMISM_SEPOLIA_REGISTRY = "OPTIMISM_SEPOLIA_REGISTRY",
-}
-
-interface ChainProps {
-  id: number;
-  name: string;
-}
-
-export const ChainInfo: Record<DeployedAddresses, ChainProps> = {
-  [DeployedAddresses.OPTIMISM_TRUSTFUL_RESOLVER]: {
-    id: 10,
-    name: "Optimism",
-  },
-  [DeployedAddresses.OPTIMISM_EAS]: {
-    id: 10,
-    name: "Optimism",
-  },
-  [DeployedAddresses.OPTIMISM_REGISTRY]: {
-    id: 10,
-    name: "Optimism",
-  },
-  [DeployedAddresses.OPSEPOLIA_TRUSTFUL_RESOLVER]: {
-    id: 11155420,
-    name: "Optimism Sepolia",
-  },
-  [DeployedAddresses.OPTIMISM_SEPOLIA_EAS]: {
-    id: 11155420,
-    name: "Optimism Sepolia",
-  },
-  [DeployedAddresses.OPTIMISM_SEPOLIA_REGISTRY]: {
-    id: 11155420,
-    name: "Optimism Sepolia",
-  },
-};
-
 export const EAS_CONTRACT_OP = "0x4200000000000000000000000000000000000021";
 
 export const RESOLVER_CONTRACT_OP =
   "0xF988953B76B92f2E15Ee5AFFd0c95925261809a9";
-
-export const TRUSTFUL_CONTRACT_ADDRESSES = {
-  [ChainInfo.OPTIMISM_TRUSTFUL_RESOLVER.id]: "0X123...",
-  [ChainInfo.OPTIMISM_EAS.id]: "0x4200000000000000000000000000000000000021",
-  [ChainInfo.OPTIMISM_REGISTRY.id]:
-    "0x4200000000000000000000000000000000000020",
-  [ChainInfo.OPSEPOLIA_TRUSTFUL_RESOLVER.id]: "0X123...",
-  [ChainInfo.OPTIMISM_SEPOLIA_EAS.id]:
-    "0x4200000000000000000000000000000000000021",
-  [ChainInfo.OPTIMISM_SEPOLIA_REGISTRY.id]:
-    "0x4200000000000000000000000000000000000020",
-};
 
 export interface Schemas {
   name: string;
