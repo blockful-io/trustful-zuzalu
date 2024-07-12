6579c197fbda5abd2ea7a7ad3bc2b2680a67b9a2
0xneves <asguilift@hotmail.com>
2024-07-12 08:54:53 +0700

    fix: linter

--- src/components/04-templates/PreCheckinSection.tsx
+++ src/components/04-templates/PreCheckinSection.tsx
@@ -1,10 +1,11 @@
+import { useContext } from "react";
+
 import { Button, Flex } from "@chakra-ui/react";
 import Image from "next/image";
 import { useRouter } from "next/navigation";
 
 import { TheFooterNavbar, TheHeader } from "@/components/01-atoms";
 import { useWindowSize } from "@/hooks";
-import { useContext } from "react";
 import { WalletContext } from "@/lib/context/WalletContext";
 
 export const PreCheckInSection = () => {
