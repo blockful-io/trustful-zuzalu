9111f3de43dfc2940927c8bba0363637c73a80a1
heronlancellot.eth <heronlancellot@id.uff.br>
2024-07-15 10:37:17 -0300

    fix: remove logic to use only production hostname to share

--- src/components/01-atoms/QRCodeGiveBadge.tsx
+++ src/components/01-atoms/QRCodeGiveBadge.tsx
@@ -1,23 +1,12 @@
 import { useQRCode } from "next-qrcode";
 import { useAccount } from "wagmi";
 
-import { useUrl } from "@/hooks/useURL";
-
 export const QRCodeGiveBadge = () => {
   const { Canvas } = useQRCode();
   const { address } = useAccount();
 
-  const url = useUrl();
-  let prefixToGiveBadge: string = "http://localhost:3000/give-badge";
-  if (url) {
-    if (url.hostname === "localhost") {
-      prefixToGiveBadge = "http://localhost:3000/give-badge";
-    } else if (url.hostname === "trustful") {
-      prefixToGiveBadge = "https://trustful.vercel.app/give-badge";
-    }
-  }
+  const prefixToGiveBadge = "https://trustful.ing/give-badge";
   const linkToGiveBadgeAddress = `${prefixToGiveBadge}?address=${address}`;
-  console.log("linkToGiveBadgeAddress", linkToGiveBadgeAddress);
 
   return (
     <Canvas
