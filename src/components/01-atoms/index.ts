156982cb9edfaf3a669acd873c873cf6efacd6a4
heronlancellot.eth <heronlancellot@id.uff.br>
2024-07-15 13:25:42 -0300

    feat: add paste-to-clipboard-button

--- src/components/01-atoms/index.ts
+++ src/components/01-atoms/index.ts
@@ -29,6 +29,7 @@ export * from "./BadgeDetailsNavigation";
 export * from "./CopyToClipboardButton";
 export * from "./CreatedByBlockful";
 export * from "./DropdownProfile";
+export * from "./PasteToClipboardButton";
 export * from "./QRCodeGiveBadge";
 export * from "./SignMessage";
 export * from "./TheFooterNavbar";
