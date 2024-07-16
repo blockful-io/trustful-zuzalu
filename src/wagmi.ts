06a0718403e93bfd07077e2c9236a21e9f8a96a2
heronlancellot.eth <heronlancellot@id.uff.br>
2024-07-16 12:38:01 -0300

    feat: only allow the optimism chain

--- src/wagmi.ts
+++ src/wagmi.ts
@@ -11,26 +11,8 @@ import {
   walletConnectWallet,
 } from "@rainbow-me/rainbowkit/wallets";
 import type { Transport } from "viem";
-// import { createClient } from "viem";
 import { createConfig, http } from "wagmi";
-import {
-  mainnet,
-  sepolia,
-  polygon,
-  polygonMumbai,
-  optimism,
-  optimismGoerli,
-  arbitrum,
-  arbitrumGoerli,
-  zkSync,
-  zkSyncSepoliaTestnet,
-  linea,
-  lineaTestnet,
-  base,
-  baseGoerli,
-  bsc,
-  bscTestnet,
-} from "wagmi/chains";
+import { optimism } from "wagmi/chains";
 
 const walletConnectProjectId = process.env.NEXT_PUBLIC_WALLETCONNECT_PROJECT_ID;
 
@@ -56,55 +38,16 @@ const connectors = connectorsForWallets(
       ],
     },
   ],
-  { appName: "Next-Web3-Boilerplate", projectId: walletConnectProjectId },
+  { appName: "Trustful", projectId: walletConnectProjectId },
 );
 
 const transports: Record<number, Transport> = {
-  [mainnet.id]: http(),
-  [sepolia.id]: http(),
-  [polygon.id]: http(),
-  [polygonMumbai.id]: http(),
   [optimism.id]: http(),
-  [optimismGoerli.id]: http(),
-  [arbitrum.id]: http(),
-  [arbitrumGoerli.id]: http(),
-  [zkSync.id]: http(),
-  [zkSyncSepoliaTestnet.id]: http(),
-  [linea.id]: http(),
-  [lineaTestnet.id]: http(),
-  [base.id]: http(),
-  [baseGoerli.id]: http(),
-  [bsc.id]: http(),
-  [bscTestnet.id]: http(),
 };
 
-// const viemClient = createClient({
-//   chain: optimism,
-//   transport: http(),
-// });
-
 export const wagmiConfig = createConfig({
-  chains: [
-    mainnet,
-    sepolia,
-    polygon,
-    polygonMumbai,
-    optimism,
-    optimismGoerli,
-    arbitrum,
-    arbitrumGoerli,
-    zkSync,
-    base,
-    baseGoerli,
-    bsc,
-    bscTestnet,
-  ],
+  chains: [optimism],
   connectors,
   transports,
   ssr: true,
-  // client: (optimism) => {
-  //   return createClient({
-  //     transport: http(),
-  //   });
-  // },
 });
