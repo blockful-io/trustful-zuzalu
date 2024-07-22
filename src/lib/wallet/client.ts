db424df987486d6a4caafb2c8f6ab0034c8c5e29
0xneves <asguilift@hotmail.com>
2024-07-23 06:28:59 +0700

    feat: crashing the application if the env is not set

--- src/lib/wallet/client.ts
+++ src/lib/wallet/client.ts
@@ -3,6 +3,13 @@ import { http } from "viem";
 import { optimism } from "viem/chains";
 import { mainnet } from "viem/chains";
 
+const mainnetRpcUrl = process.env.ALCHEMY_RPC_URL;
+if (!mainnetRpcUrl) {
+  throw new Error(
+    "Mainnet RPC URL is not defined. Please check your environment variables.",
+  );
+}
+
 export const publicClient = createPublicClient({
   chain: optimism,
   transport: http(),
@@ -15,5 +22,5 @@ export const walletClient = createWalletClient({
 
 export const publicClientMainnet = createPublicClient({
   chain: mainnet,
-  transport: http(process.env.ALCHEMY_RPC_URL),
+  transport: http(mainnetRpcUrl),
 });
